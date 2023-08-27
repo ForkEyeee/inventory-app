9c30fa4b52fadd41d05254d1831246959d7db608
ForkEyeee <shawncarter123456@gmail.com>
2023-08-27 18:10:48 -0400

    working on models and controllers for category and item

---
+++ dist/models/item.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ItemSchema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    description: { type: String, required: true, maxLength: 200 },
+    cateogory: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    price: { type: mongoose.Decimal128, required: true },
+    quanitity: { type: Number, required: true },
+});
+ItemSchema.virtual("url").get(function () {
+    // We don't use an arrow function as we'll need the this object
+    return `/catalog/genre/${this._id}`;
+});
+module.exports = mongoose.model("Item", ItemSchema);
